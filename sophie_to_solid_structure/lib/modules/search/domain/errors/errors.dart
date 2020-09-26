8b4d339cbdd3d42819ed81d9948623f1b8064547
Rodrigo Morais Rodarte <rodarte.rodrigo@hotmail.com>
2020-09-26 17:11:09 -0300

    adjust in FailureSearch

--- sophie_to_solid_structure/lib/modules/search/domain/errors/errors.dart
+++ sophie_to_solid_structure/lib/modules/search/domain/errors/errors.dart
@@ -3,6 +3,12 @@ abstract class FailureSearch implements Exception{}
 class InvalidTextError implements FailureSearch{}
 
 class DataSourceError implements FailureSearch{
-  final String message;
+  final dynamic message;
   DataSourceError({this.message});
+
+  String toString() {
+    Object message = this.message;
+    if (message == null) return "DataSourceError";
+    return "DataSourceError: $message";
+  }
 }
